75b696ff25a3cab7c99f0aa835611d159c2fa4e1
Zach Renwick <zach.renwick@gmail.com>
2024-12-18 00:56:53 +0000

    add google analytics tagging

---
+++ pages/+layout.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SH8P4T8C3W"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-SH8P4T8C3W');
+  </script>
+</svelte:head>
