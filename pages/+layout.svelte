2753a733da32fb16d0c901f5af8172288afc4793
Zach Renwick <zach.renwick@gmail.com>
2024-12-19 17:15:28 +0000

    fix layout

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -1,11 +1,28 @@
-<svelte:head>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SH8P4T8C3W"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
 
-    gtag('config', 'G-SH8P4T8C3W');
-  </script>
-</svelte:head>
+    import { onMount } from 'svelte';
+// Dynamically import the Google Tag Manager script
+onMount(async () => {
+    const gtagScript = document.createElement('script');
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-SH8P4T8C3W";
+    gtagScript.async = true;
+    document.head.appendChild(gtagScript);
+
+    gtagScript.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-SH8P4T8C3W');
+    };
+});
+
+
+</script>
+
+<EvidenceDefaultLayout {data}>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
